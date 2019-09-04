e9651efa40b9f0d3d59de3272d1f5e1ced3f64f3
Allison Lucas <allison@mojotech.com>
2019-09-04 12:10:57 -0600

    App initialized.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [dogPic, setDogPic] = useState(null);
+  const [dogBreeds, setDogBreeds] = useState(null);
+  const [showBreedInfo, setShowBreedInfo] = useState(false);
+
+  const getDogPic = () => {
+    fetch("https://api.thedogapi.com/v1/images/search", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "2f6fbd8c-350b-42fb-b93b-cf8a95c6712f"
+      }
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log("json: ", myJson);
+        setDogPic(myJson[0].url);
+      });
+  };
+
+  const getDogBreeds = () => {
+    fetch("https://api.thedogapi.com/v1/breeds", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "2f6fbd8c-350b-42fb-b93b-cf8a95c6712f"
+      }
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log("json: ", myJson);
+        setDogBreeds(myJson);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h2>All About Dogs</h2>
+        {dogPic && <img src={dogPic} width={300} />}
+        <button
+          style={{
+            backgroundColor: "#add8e6",
+            color: "#282c34",
+            padding: "1rem",
+            borderRadius: "3px",
+            border: "none",
+            cursor: "pointer",
+            fontSize: "1.2rem",
+            fontWeight: "500",
+            margin: "1rem"
+          }}
+          onClick={getDogPic}
         >
-          Learn React
-        </a>
+          {dogPic ? "Get Another Dog Picture" : "Get Dog Picture"}
+        </button>
+
+        {dogBreeds &&
+          dogBreeds.map((breed, i) => (
+            <div key={i}>
+              <p>{breed.name}</p>
+              <button onClick={() => setShowBreedInfo(!showBreedInfo)}>
+                More Info
+              </button>
+              {showBreedInfo && (
+                <table style={{ width: "100%" }}>
+                  <tbody style={{ display: "flex", flexDirection: "column" }}>
+                    <tr style={{ display: "flex" }}>
+                      <th style={{ flex: 1 }}>Temperament</th>
+                      <th style={{ flex: 1 }}>Bred For</th>
+                      <th style={{ flex: 1 }}>Breed Type</th>
+                      <th style={{ flex: 1 }}>Life Span</th>
+                    </tr>
+                    <tr style={{ display: "flex" }}>
+                      <td style={{ flex: 1 }}>{breed.temperament}</td>
+                      <td style={{ flex: 1 }}>{breed.bred_for}</td>
+                      <td style={{ flex: 1 }}>{breed.breed_group}</td>
+                      <td style={{ flex: 1 }}>{breed.life_span}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              )}
+            </div>
+          ))}
+        <button onClick={getDogBreeds}>Get Dog Breeds</button>
       </header>
     </div>
   );
